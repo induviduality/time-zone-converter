7d54222cf44964ff0f41cfe2aad8e719425f295d
Induja <53386868+indujaaaa@users.noreply.github.com>
2021-07-28 14:03:22 +0530

    Clean-up

--- send_email.py
+++ send_email.py
@@ -1,17 +1,14 @@
-# using SendGrid's Python Library
-# https://github.com/sendgrid/sendgrid-python
-
 import os
 import sendgrid
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 message = Mail(
-    from_email='induja.shankar@gmail.com',
-    to_emails='induja.exe@gmail.com',
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    from_email='abcxyz@gmail.com',
+    to_emails='receiver@gmail.com',
+    subject='Hello from the other side',
+    html_content='<strong>I must have called a thousand times</strong>')
 
-sg = sendgrid.SendGridAPIClient(api_key='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')
+sg = sendgrid.SendGridAPIClient(api_key='totallyNotAnAPIKey')
 response = sg.send(message)
 print(response.status_code, response.body, response.headers)
